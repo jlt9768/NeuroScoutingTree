5d083c43b87ea9dc5a9d2557099701ed1786fadc
James Troup <jlt9768@g.rit.edu>
2017-10-25 19:52:46 -0500

    Start of looping through to create node levels

--- TreeExercise/TreeExercise/Tree.cs
+++ TreeExercise/TreeExercise/Tree.cs
@@ -11,7 +11,7 @@ namespace TreeExercise
         private Node root = new Node();
         public Node Root
         {
-            get { return root;}
+            get { return root; }
         }
 
         /// <summary>
@@ -24,6 +24,25 @@ namespace TreeExercise
             {
                 Console.WriteLine("Depth needs to be great than or equal to 1");
                 depth = Convert.ToInt32(Console.ReadLine());
+            }
+            //Current Level
+            int cLevel = 2;
+            //Current level of nodes within the tree
+            List<Node> currentLevel = new List<Node>();
+            //Next level of nodes within the tree
+            List<Node> nextLevel = new List<Node>();
+
+
+            currentLevel.Add(root);
+            Console.WriteLine(root.Value);
+
+            //Keep track of the last node created to keep track of neighbors
+            Node last;
+
+            //Loop through to the depth wanted for the tree and don't loop if depth is equal to 1
+            while (cLevel <= depth && depth > 1)
+            {
+
             }
         }
     }
